0ab78dab658782ed826b19f136d4be7ab6a4a88b
jihadrbaiti <jihadrbaiti14@gmail.com>
2025-06-20 22:33:09 +0100

    m

--- DeepSeek/cpo_atlas_cos.py
+++ DeepSeek/cpo_atlas_cos.py
@@ -14,7 +14,13 @@ import wandb
 from torch.optim import RMSprop
 from peft import LoraConfig, get_peft_model
 #from peft import print_trainable_parameters
+from transformers import AutoConfig
 
+
+
+config = AutoConfig.from_pretrained("deepseek-ai/DeepSeek-R1-0528", trust_remote_code=True)
+if hasattr(config, "quantization_config"):
+    config.quantization_config = None
 
 peft_config = LoraConfig(
     r=8,  # Low-rank dimension (try 8 or 16)
@@ -24,7 +30,7 @@ peft_config = LoraConfig(
     task_type="CAUSAL_LM",  # since you're using a decoder-only model
 )
 
-base_model = AutoModelForCausalLM.from_pretrained("deepseek-ai/DeepSeek-R1-0528", torch_dtype=torch.bfloat16, revision="main", trust_remote_code=True)
+base_model = AutoModelForCausalLM.from_pretrained("deepseek-ai/DeepSeek-R1-0528", torch_dtype=torch.bfloat16, config=config, trust_remote_code=True)
 tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/DeepSeek-R1-0528", trust_remote_code=True)
 model = get_peft_model(base_model, peft_config)
 #wandb.init(id='wf803ujj', resume='must', project="huggingface", name="ATLAS_cos" )
