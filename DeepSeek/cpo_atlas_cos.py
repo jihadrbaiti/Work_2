2dd291061b2e6001f3c3da355a8c31f661f2ae83
jihadrbaiti <jihadrbaiti14@gmail.com>
2025-06-20 22:28:50 +0100

    m

--- DeepSeek/cpo_atlas_cos.py
+++ DeepSeek/cpo_atlas_cos.py
@@ -24,8 +24,8 @@ peft_config = LoraConfig(
     task_type="CAUSAL_LM",  # since you're using a decoder-only model
 )
 
-base_model = AutoModelForCausalLM.from_pretrained("deepseek-ai/DeepSeek-R1-0528", torch_dtype=torch.bfloat16, trust_remote_code=True`)
-tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/DeepSeek-R1-0528", trust_remote_code=True`)
+base_model = AutoModelForCausalLM.from_pretrained("deepseek-ai/DeepSeek-R1-0528", torch_dtype=torch.bfloat16, trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/DeepSeek-R1-0528", trust_remote_code=True)
 model = get_peft_model(base_model, peft_config)
 #wandb.init(id='wf803ujj', resume='must', project="huggingface", name="ATLAS_cos" )
 
