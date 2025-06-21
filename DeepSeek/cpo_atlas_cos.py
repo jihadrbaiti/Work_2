6cc10830d5b775bc0b7cdfebd2505c8369455574
jihadrbaiti <jihadrbaiti14@gmail.com>
2025-06-21 01:41:40 +0100

    m

--- DeepSeek/cpo_atlas_cos.py
+++ DeepSeek/cpo_atlas_cos.py
@@ -36,9 +36,9 @@ model = get_peft_model(base_model, peft_config)
 
 #wandb.init(id='wf803ujj', resume='must', project="huggingface", name="ATLAS_cos" )
 
-'''if tokenizer.pad_token is None:
+if tokenizer.pad_token is None:
     tokenizer.pad_token = tokenizer.eos_token
-if tokenizer.chat_template is None:
+'''if tokenizer.chat_template is None:
     tokenizer.chat_template = SIMPLE_SFT_CHAT_TEMPLATE'''
 
 wandb.login(key='86570a60523435fb4d496c0e63e8ae11c308bae2')
