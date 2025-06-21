2d4ecf8db718d53bce619137415e7dd5c1f8f4dd
jihadrbaiti <jihadrbaiti14@gmail.com>
2025-06-21 02:01:43 +0100

    m

--- run_file.sh
+++ run_file.sh
@@ -9,4 +9,4 @@ conda activate geo_ai
 
 echo "Starting training of deepseek/xalma_atlas_cos.py on two gpu"
  
-CUDA_VISIBLE_DEVICES=1,2,3,4 accelerate launch --main_process_port=29525 --num_processes=3 DeepSeek/cpo_atlas_cos.py  > "$LOG_DIR/training_xalma_cos_deepseek_out.log" 2> "$LOG_DIR/training_xalma_cos_deepseek_err.log"
+CUDA_VISIBLE_DEVICES=1,2,3,4 accelerate launch --main_process_port=29526 --num_processes=3 DeepSeek/cpo_atlas_cos.py  > "$LOG_DIR/training_xalma_cos_deepseek_out.log" 2> "$LOG_DIR/training_xalma_cos_deepseek_err.log"
